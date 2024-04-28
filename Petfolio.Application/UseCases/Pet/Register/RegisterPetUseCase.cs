e62840ea77d7945a20f641da0321f6c471c3d019
rodolfostark <rodolfojonatas@outlook.com>
2024-04-28 12:04:05 -0300

    feat: add use case register pet

---
+++ Petfolio.Application/UseCases/Pet/Register/RegisterPetUseCase.cs
@@ -0,0 +1,15 @@
+﻿using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.Application.UseCases.Pet.Register;
+public class RegisterPetUseCase
+{
+    public ResponseRegisteredPetJson Execute(RequestRegisterPetJson request)
+    {
+        return new ResponseRegisteredPetJson
+        {
+            Id = 6,
+            Name = request.Name,
+        };
+    }
+}
